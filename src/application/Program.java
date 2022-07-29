dd684b2a3ac129cfb4f70edc154cb5edc0a9ca7f
luciosantiagovanderlei <luciosantiagovandelei@gmail.com>
2022-07-29 18:10:09 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.Daofactory;
 import model.dao.SellerDao;
@@ -12,7 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-				
+		Scanner sc = new Scanner(System.in);	
 			
 		SellerDao sellerDao = Daofactory.createSellerdao();
 		
@@ -46,6 +47,14 @@ public class Program {
 		seller.setName("Marta Whayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("\n ===== TEST 6: SELLER Delete ======");
+		
+		System.out.println(" Enter id for delete: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete complete");
+		sc.close();
 	}
 	
 
